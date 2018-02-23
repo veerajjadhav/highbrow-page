0afe0a2a72c47ad863006221218188b433ede0f5
veeraj <veerajjadhav1991@gmail.com>
2018-02-23 16:29:58 +0530

    active change

--- js/custom.js
+++ js/custom.js
@@ -2,10 +2,7 @@ jQuery( document ).ready( function () {
     function scrollNav() {
         $( '.de-modules a' ).click( function () {
             //Toggle Class
-            $( ".active" ).removeClass( "active" );
-            $( this ).closest( 'li' ).addClass( "active" );
-            var theClass = $( this ).attr( "class" );
-            $( '.' + theClass ).parent( 'li' ).addClass( 'active' );
+
             //Animate
             $( 'html, body' ).stop().animate( {
                 scrollTop: $( $( this ).attr( 'href' ) ).offset().top - 0
